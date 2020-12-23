dfb112c179a55bb9add30abafc464e8ecef77a95
Kevin Kennis <kevinvkennis@gmail.com>
2020-12-23 14:45:34 -0800

    re-do dir structure

--- packages/client/src/services/api.js
+++ packages/client/src/services/api.js
@@ -1,26 +1,26 @@
 
 export class UniswapApiFetcher {
     static async getPairOverview(pairId) {
-        const response = await fetch(`api/v1/uniswap/pairs/${pairId}`);
+        const response = await fetch(`/api/v1/uniswap/pairs/${pairId}`);
         const { data } = await response.json();
         return data;
     }
 
     static async getLatestSwaps(pairId) {
-        const response = await fetch(`api/v1/uniswap/pairs/${pairId}/swaps`);
+        const response = await fetch(`/api/v1/uniswap/pairs/${pairId}/swaps`);
         const { data } = await response.json();
         return data;
     }
 
 
     static async getTopPairs(count = 1000) {
-        const response = await fetch(`api/v1/uniswap/pairs?count=${count}`);
+        const response = await fetch(`/api/v1/uniswap/pairs?count=${count}`);
         const { data } = await response.json();
         return data;
     }
 
     static async getHistoricalDailyData(pairId, startDate, endDate = new Date()) {
-        const response = await fetch(`api/v1/uniswap/historical/${pairId}?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}`);
+        const response = await fetch(`/api/v1/uniswap/historical/${pairId}?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}`);
         const { data } = await response.json();
         return data;
     }
