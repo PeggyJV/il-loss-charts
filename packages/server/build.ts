07a91eeee92c4b294a22aa45b27f29d324d5078a
Kevin Kennis <kevinvkennis@gmail.com>
2020-12-23 18:45:44 -0800

    get build

--- packages/server/build.ts
+++ packages/server/build.ts
@@ -6,4 +6,4 @@ s.rm('-rf', outDir);
 s.mkdir(outDir);
 s.cp('.env', `${outDir}/.env`);
 s.mkdir('-p', `${outDir}/common/swagger`);
-s.cp('server/common/api.yml', `${outDir}/common/api.yml`);
+s.cp('src/common/api.yml', `${outDir}/common/api.yml`);
