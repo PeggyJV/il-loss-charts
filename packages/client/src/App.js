815653a09903448c2a1f82a9fdc2371d0e41e39f
Kevin Kennis <kevinvkennis@gmail.com>
2020-12-23 20:16:42 -0800

    using ga

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -4,10 +4,12 @@ import 'styles/app.scss';
 
 import { Container, Row, Col } from 'react-bootstrap';
 import AppContainer from 'containers/app-container';
+import GA, { init as initGA } from 'util/google-analytics';
 
 function App() {
     return (
         <div className="app">
+            { initGA() && <GA />}
             <Container fluid>
                 <Row>
                     <Col><h2 className="page-title">Uniswap Impermanent Loss Calculator</h2></Col>
---
+++ packages/client/src/util/google-analytics.js
@@ -0,0 +1,62 @@
+// utils/GoogleAnalytics.js
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import ReactGA from 'react-ga';
+import { Route } from 'react-router-dom';
+
+export default function GoogleAnalytics({ location }) {
+    function logPageChange(pathname, search = '') {
+        const page = pathname + search;
+        const { location } = window;
+        ReactGA.set({
+            page,
+            location: `${location.origin}${page}`,
+            ...this.props.options
+        });
+        ReactGA.pageview(page);
+    }
+
+    useEffect(() => {
+        logPageChange(
+            location.pathname,
+            location.search
+        );
+    }, []);
+
+    useEffect(() => {
+        const { pathname, search } = location
+        const isDifferentPathname = pathname !== prevLocation.pathname;
+        const isDifferentSearch = search !== prevLocation.search;
+
+        if (isDifferentPathname || isDifferentSearch) {
+            logPageChange(pathname, search);
+        }
+    }, [location]);
+
+    const prevLocationRef = useRef();
+    useEffect(() => {
+        prevLocationRef.current = location;
+    });
+    const prevLocation = prevLocationRef.current;
+
+    return null;
+}
+
+GoogleAnalytics.propTypes = {
+    location: PropTypes.shape({
+        pathname: PropTypes.string,
+        search: PropTypes.string
+    }).isRequired,
+    options: PropTypes.object
+};
+
+export const init = (options = {}) => {
+    const isGAEnabled = process.env.NODE_ENV === 'production';
+
+    if (isGAEnabled) {
+        console.log('Enabling google analytics');
+        ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+    }
+
+    return isGAEnabled;
+};
