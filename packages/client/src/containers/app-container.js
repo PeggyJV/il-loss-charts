a7b93f376a605753f912c0fc6e240b126cf8ad2f
Kevin Kennis <kevinvkennis@gmail.com>
2020-12-30 13:10:35 -0800

    add join link

--- packages/client/src/containers/app-container.js
+++ packages/client/src/containers/app-container.js
@@ -156,6 +156,14 @@ function ChartsContainer() {
     return (
         <Container fluid>
             <Row>
+                <Col>
+                    <h6 className="social-cta">
+                        <a href="https://t.me/getsomm">Join the Sommelier Community</a>
+                    </h6>
+                </Col>
+            </Row>
+
+            <Row>
                 <Col><h2 className="page-title">Uniswap Impermanent Loss Calculator</h2></Col>
             </Row>
             <Row noGutters>
