17017ad4a4996233fa864503813c9132f34ff048
Kevin Kennis <kevinvkennis@gmail.com>
2021-01-03 14:42:38 -0800

    add basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-widgets/dist/css/react-widgets.css';
 import 'styles/app.scss';
 
-import AppContainer from 'containers/app-container';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import PairContainer from 'containers/pair-container';
 
 function App() {
     return (
-        <div className="app">
-            <AppContainer />
-        </div>
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Route path='/'>
+                    </Route>
+                    <Route path='/pair/:id'>
+                        <PairContainer />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
