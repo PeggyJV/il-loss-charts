a066a9a5c491b2bffd921883e37ffb4e15e64203
Kevin Kennis <kevinvkennis@gmail.com>
2021-01-03 20:38:24 -0800

    working on landing

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,59 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-widgets/dist/css/react-widgets.css';
 import 'styles/app.scss';
 
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+import AppContext from 'util/app-context';
 import PairContainer from 'containers/pair-container';
 
+import { UniswapApiFetcher as Uniswap } from 'services/api';
+import { calculatePairRankings } from 'services/calculate-lp-stats';
+
+import initialData from 'constants/initialData.json';
+
+
 function App() {
+    // ------------------ Initial Mount - API calls for first render ------------------
+
+    const [allPairs, setAllPairs] = useState({ isLoading: true, pairs: initialData.allPairs });
+
+    useEffect(() => {
+        const fetchAllPairs = async () => {
+            // Fetch all pairs
+            const pairsRaw = await Uniswap.getTopPairs();
+            const calculated = calculatePairRankings(pairsRaw);
+            setAllPairs({ isLoading: false, pairs: calculated.pairs, lookups: calculated.pairLookups });
+        }
+        fetchAllPairs();
+    }, []);
+
+
+    if (allPairs.isLoading) {
+        return (
+            <Container className="loading-container">
+                <div className='wine-bounce'>🍷</div>
+            </Container>
+        );
+    }
+
     return (
-        <Router>
-            <div className="app">
-                <Switch>
-                    <Route path='/'>
-                    </Route>
-                    <Route path='/pair/:id'>
-                        <PairContainer />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+        <AppContext.Provider value={{ allPairs }}>
+            <Router>
+                <div className="app">
+                    <Switch>
+                        <Route path='/pair/:id'>
+                            <PairContainer />
+                        </Route>
+                        <Route path='/'>
+                            <div>Hello World</div>
+                        </Route>
+                    </Switch>
+                </div >
+            </Router>
+        </AppContext.Provider >
     );
 }
 
---
+++ client/src/containers/overview-container.js
@@ -0,0 +1,5 @@
+function OverviewContainer() {
+
+}
+
+export default OverviewContainer;
