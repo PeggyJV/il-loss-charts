1041dfc1719cae4e3114a7f7a0628d39e8b1ec01
Kevin Kennis <kevinvkennis@gmail.com>
2021-01-05 23:16:17 -0800

    ts working on build and prod run

--- packages/server/src/services/index.ts
+++ packages/server/src/services/index.ts
@@ -1,11 +1,9 @@
 import uniswap from './uniswap';
 import infura from './infura';
-import alchemy from './alchemy';
 
 export default {
     uniswap,
     infura,
-    alchemy
 };
 
-export type DataSource = 'uniswap' | 'infura' | 'alchemy';
+export type DataSource = 'uniswap' | 'infura';
