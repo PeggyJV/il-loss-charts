5362cd372337203a81b58d2fa67193d2d2eed5f0
Kevin Kennis <kevinvkennis@gmail.com>
2021-02-01 13:10:55 -0800

    add sentry

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import './index.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+    dsn:
+        'https://f35d9c09635541b1803ab440224ae61f@o514714.ingest.sentry.io/5618374',
+    integrations: [new Integrations.BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
     <React.StrictMode>
