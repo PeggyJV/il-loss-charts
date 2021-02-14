d9789700637a9fdae96330042ef2a8fbd428a3e9
Kevin Kennis <kevinvkennis@gmail.com>
2021-02-13 23:04:44 -0800

    fix redis user

--- packages/server/src/config/default.ts
+++ packages/server/src/config/default.ts
@@ -3,7 +3,7 @@ import AppConfig from 'types/app-config';
 const config: AppConfig = {
     redisHost: process.env.REDIS_URL || '127.0.0.1',
     redisPort: 6379,
-    redisUser: 'sommeliers',
+    redisUser: 'sommelier',
 };
 
 export default config;
