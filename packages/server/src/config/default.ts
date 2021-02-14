17009aaca9f483ceca25d018e9cbe9f0e35f52bf
Kevin Kennis <kevinvkennis@gmail.com>
2021-02-14 00:27:31 -0800

    get redis config working

--- packages/server/src/config/default.ts
+++ packages/server/src/config/default.ts
@@ -3,7 +3,6 @@ import AppConfig from 'types/app-config';
 const config: AppConfig = {
     redisHost: process.env.REDIS_URL || '127.0.0.1',
     redisPort: 6379,
-    redisUser: 'sommelier',
 };
 
 export default config;
--- packages/server/src/types/app-config.ts
+++ packages/server/src/types/app-config.ts
@@ -3,5 +3,4 @@ export type Environments = 'production' | 'development' | 'test' | 'staging';
 export default interface AppConfig {
     redisHost: string;
     redisPort: number;
-    redisUser: string;
 }
