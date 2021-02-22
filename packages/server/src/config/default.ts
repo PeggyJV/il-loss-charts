13184a96e71a2f1a19faa144945ceac102515fe4
Kevin Kennis <kevinvkennis@gmail.com>
2021-02-21 18:39:57 -0800

    fix redis port config

--- packages/server/src/config/default.ts
+++ packages/server/src/config/default.ts
@@ -2,7 +2,9 @@ import AppConfig from 'types/app-config';
 
 const config: AppConfig = {
     redisHost: process.env.REDIS_URL || '127.0.0.1',
-    redisPort: 6379,
+    redisPort: process.env.REDIS_PORT
+        ? parseInt(process.env.REDIS_PORT, 10)
+        : 6379,
 };
 
 export default config;
