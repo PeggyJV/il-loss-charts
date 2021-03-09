e53dfa1ded6a2ca18e1674159fa30f8d63925875
Kevin Kennis <kevinvkennis@gmail.com>
2021-03-09 10:56:53 -0800

    add entrypoint and redis connection url

--- packages/server/src/config/production.ts
+++ packages/server/src/config/production.ts
@@ -1,5 +1,8 @@
 import AppConfig from 'types/app-config';
 
-const config: Partial<AppConfig> = {};
+const config: Partial<AppConfig> = {
+    redisHost: '10.33.17.4',
+    redisPort: 6378
+};
 
 export default config;
