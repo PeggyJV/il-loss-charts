ea311924efa65c12ca7bc25eab94a9285684c1f8
Kevin Kennis <kevinvkennis@gmail.com>
2021-03-09 10:58:33 -0800

    authenticate w redis

--- packages/server/src/util/redis.ts
+++ packages/server/src/util/redis.ts
@@ -4,6 +4,9 @@ import Redis from 'ioredis';
 const redis = new Redis({
     port: config.redisPort,
     host: config.redisHost,
+    password: process.env.REDIS_AUTH
 });
+
+// Authenticate with redis
 
 export default redis;
