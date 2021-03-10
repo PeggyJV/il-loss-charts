c929ba32e1ba8d20e9d183237e2374d5c624e566
Kevin Kennis <kevinvkennis@gmail.com>
2021-03-09 16:14:16 -0800

    change redis connection

--- packages/server/src/config/production.ts
+++ packages/server/src/config/production.ts
@@ -2,7 +2,7 @@ import AppConfig from 'types/app-config';
 
 const config: Partial<AppConfig> = {
     redisHost: '10.33.17.4',
-    redisPort: 6378
+    redisPort: 6379
 };
 
 export default config;
