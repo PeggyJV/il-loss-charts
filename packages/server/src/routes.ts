955204da7b7b83ce81424e6b4080c5b6978a3277
Kevin Kennis <kevinvkennis@gmail.com>
2021-03-09 19:46:36 -0800

    add healthcheck

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,5 +1,9 @@
 import { Application } from 'express';
 import uniswapRouter from './api/controllers/uniswap';
 export default function routes(app: Application): void {
+    app.get('/api/v1/healthcheck', (req, res) => {
+        res.send('alive');
+    });
+    
     app.use('/api/v1/uniswap', uniswapRouter);
 }
