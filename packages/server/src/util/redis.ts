b7250bf80085bd399e8e7cfbd7826aa8027a1de9
Kevin Kennis <kevinvkennis@gmail.com>
2021-03-09 18:23:39 -0800

    better redis error logging

--- packages/server/src/util/redis.ts
+++ packages/server/src/util/redis.ts
@@ -1,11 +1,18 @@
 import config from 'config';
 import Redis from 'ioredis';
 
-const redis = new Redis({
-    port: config.redisPort,
-    host: config.redisHost,
-    password: process.env.REDIS_AUTH
-});
+let redis: Redis.Redis;
+
+try {
+    redis = new Redis({
+        port: config.redisPort,
+        host: config.redisHost,
+        password: process.env.REDIS_AUTH
+    });
+} catch (e) {
+    console.error(`Error connecting to redis at ${config.redisHost}:${config.redisPort}: ${e.message as string}`);
+    throw e;
+}
 
 // Authenticate with redis
 
