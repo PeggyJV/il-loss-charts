c51433a3d4994f3eaea7aa36dd9c6f5e6425434c
Jason Jacobs <nerfnerd@gmail.com>
2021-03-29 23:52:30 -0700

    Add a telegram mixpanel bot

---
+++ packages/workers/src/scripts/telegram-mixpanel.ts
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import Mixpanel from 'mixpanel';
+let mixpanel: Mixpanel.Mixpanel;
+
+import TelegramBot from 'node-telegram-bot-api';
+let sommBot: TelegramBot | undefined;
+
+
+
+if(process.env.SOMM_STATS_BOT_TOKEN){
+  sommBot = new TelegramBot(process.env.SOMM_STATS_BOT_TOKEN, {polling: true});
+} else {
+    throw new Error(`Cannot start telegram mixpanel stat bot without stat bot token.`);
+}
+
+if (process.env.MIXPANEL_TOKEN) {
+    mixpanel = Mixpanel.init(process.env.MIXPANEL_TOKEN);
+} else {
+    throw new Error(`Cannot start telegram mixpanel stat bot without mixpanel token.`);
+}
+
+sommBot.on('channel_post', (msg: TelegramBot.Message) => {
+  mixpanel.track('telegram:message', {
+      distinct_id: msg.message_id
+  });
+});
+
+sommBot.on('new_chat_members', (msg: TelegramBot.Message) => {
+  mixpanel.track('telegram:user:join', {
+      distinct_id: msg.message_id
+  });
+});
+
+
+sommBot.on('left_chat_member', (msg: TelegramBot.Message) => {
+  mixpanel.track('telegram:user:leave', {
+      distinct_id: msg.message_id
+  });
+});
