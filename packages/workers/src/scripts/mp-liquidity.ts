13bc66cc81f6f26ae7d1bcd71f51042faa2ba283
Jason Jacobs <nerfnerd@gmail.com>
2021-04-13 13:51:42 -0700

    Add logging for workers.

--- packages/workers/src/scripts/mp-liquidity.ts
+++ packages/workers/src/scripts/mp-liquidity.ts
@@ -6,7 +6,7 @@ let mixpanel: Mixpanel.Mixpanel;
 import fetch from 'node-fetch';
 
 const MS_PER_MINUTE = 60000;
-const key = process.env.ETHPLORER_API_KEY || 'freekey';
+const key = 'freekey';
 
 const ADD_LIQUIDITY_ADDR = '0xFd8A61F94604aeD5977B31930b48f1a94ff3a195';
 const REMOVE_LIQUIDITY_ADDR = '0x418915329226AE7fCcB20A2354BbbF0F6c22Bd92';
@@ -38,16 +38,23 @@ async function getTransactionData(transactionType: string, hash: string): Promis
   const addPath ='https://api.ethplorer.io/getAddressTransactions/';
   const fullPath = `${addPath}${hash}?apiKey=${
       key
-  }&limit=200&timestamp=${
-    oneHoursBefore
-  }`
+  }&limit=200`
 
   const res = await fetch(fullPath);
+
+  console.log('*************');
+  console.log(oneHoursBefore);
+  console.log('*************');
+  console.log(res);
+  console.log('*************');
+
   const data = await res.json();
 
   data.forEach((addressTransaction: AddressTransaction) => {
     const txTimestamp = new Date(addressTransaction.timestamp * 1000);
-    
+
+    console.log(txTimestamp.getTime());
+
     if(txTimestamp.getTime() > oneHoursBefore) {
       mixpanel.import(`Uniswap:${transactionType}`,addressTransaction.timestamp,  {
         distinct_id: addressTransaction.from,
@@ -61,6 +68,7 @@ async function getTransactionData(transactionType: string, hash: string): Promis
       });
     }
   });
+  console.log('*************');
 }
 
 export default async function getTransactionDataForMixpanel(): Promise<void> {
--- packages/workers/src/scripts/telegram-mixpanel.ts
+++ packages/workers/src/scripts/telegram-mixpanel.ts
@@ -21,6 +21,8 @@ export default async function getTelegramDataForMixpanel(): Promise<void> {
   }
 
   sommBot.on('message', (msg: TelegramBot.Message) => {
+    console.log('sommBot.on message');
+    console.log(msg);
     mixpanel.track('telegram:message', {
         distinct_id: msg?.from?.id,
         user: msg?.from?.username
@@ -28,6 +30,8 @@ export default async function getTelegramDataForMixpanel(): Promise<void> {
   });
 
   sommBot.on('channel_post', (msg: TelegramBot.Message) => {
+    console.log('sommBot.on channel_post');
+    console.log(msg);
     mixpanel.track('telegram:message', {
         distinct_id: msg.message_id
     });
@@ -41,9 +45,36 @@ export default async function getTelegramDataForMixpanel(): Promise<void> {
   });
 
   sommBot.on('left_chat_member', (msg: TelegramBot.Message) => {
+    console.log('sommBot.on left_chat_member');
+    console.log(msg);
     mixpanel.track('telegram:user:leave', {
       distinct_id: msg?.from?.id,
       user: msg?.from?.username
+    });
+  });
+
+  sommBot.on('error', (error: Error) => {
+    console.log('sommBot.on error');
+    console.log(error);
+    mixpanel.track('telegram:error', {
+      error: error
+    });
+  });
+
+  sommBot.on('polling_error', (error: Error) => {
+    console.log('sommBot.on polling_error');
+    console.log(error);
+    mixpanel.track('telegram:polling_error', {
+      error: error
+    });
+  });
+
+
+  sommBot.on('webhook_error', (error: Error) => {
+    console.log('sommBot.on webhook_error');
+    console.log(error);
+    mixpanel.track('telegram:webhook_error', {
+      error: error
     });
   });
 }
