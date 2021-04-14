e80b2e5abf132bc38c722316f5469ded4945b5ca
Lucky Odisetti <lucky.odisetti@gmail.com>
2021-04-14 02:45:45 -0700

    WIP:pair search latency

--- packages/client/src/components/pair-search.tsx
+++ packages/client/src/components/pair-search.tsx
@@ -1,25 +1,27 @@
 import { useEffect, useState, useMemo } from 'react';
-import { Combobox } from 'react-widgets';
+// import { Combobox } from 'react-widgets';
 
 import { UniswapPair } from '@sommelier/shared-types';
 
 import { PairWithLogo } from 'components/token-with-logo';
-
+import { matchSorter } from 'match-sorter';
+import { useThrottle } from 'react-use';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+    ComboboxOptionText,
+} from '@reach/combobox';
+import { keys } from 'highcharts';
+import { SearchSource } from '@jest/core';
 type SearchKeys = { [searchKey: string]: UniswapPair };
 
-function PairSearch({
-    pairs,
-    setPair,
-}: {
-    pairs: UniswapPair[];
-    setPair: (pairId: string) => void;
-}): JSX.Element {
-    const [currentValue, setCurrentValue] = useState<UniswapPair | null>(null);
-
-    const searchKeys: SearchKeys = useMemo(
+function useMemoizedPairs(pairs: UniswapPair[]): SearchKeys {
+    return useMemo(
         (): SearchKeys =>
             pairs.reduce((acc: SearchKeys, pair: UniswapPair) => {
                 const address = pair.id.toLowerCase();
@@ -31,33 +33,58 @@ function PairSearch({
             }, {}),
         [pairs]
     );
+}
 
+function usePairMatch(searchValue: string, pairs: UniswapPair[]) {
+    const throttledTerm = useThrottle(searchValue, 100);
+    return useMemo(
+        () =>
+            matchSorter(pairs, searchValue, {
+                threshold: matchSorter.rankings.STARTS_WITH,
+                keys: ['token0.symbol'],
+            }),
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [throttledTerm]
+    );
+}
+
+function PairSearch({
+    pairs,
+    setPair,
+}: {
+    pairs: UniswapPair[];
+    setPair: (pairId: string) => void;
+}): JSX.Element {
+    const [currentValue, setCurrentValue] = useState<string>('');
+    const memoizedPairs = useMemoizedPairs(pairs);
+    const matchingPairs = usePairMatch(currentValue, pairs);
     // TODO: allow searching by token name or address
     // Update actual pair if current value matches a given ID
     useEffect(() => {
         if (typeof currentValue === 'string') {
-            if (searchKeys[currentValue]) {
-                setPair(searchKeys[currentValue].id);
+            if (memoizedPairs[currentValue]) {
+                setPair(memoizedPairs[currentValue].id);
             }
 
             return;
         }
 
-        if (currentValue?.id) setPair(currentValue.id);
+        // if (currentValue?.id) setPair(currentValue.id);
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentValue, searchKeys]);
+    }, [currentValue]);
 
-    const lookForPair = (pair: UniswapPair, value: string) => {
-        const search = value.toLowerCase();
-        const symbol0 = pair.token0.symbol.toLowerCase();
-        const symbol1 = pair.token1.symbol.toLowerCase();
-        const address = pair.id.toLowerCase();
+    // const lookForPair = (pair: UniswapPair, value: string) => {
+    //     console.log('look for pair');
+    //     const search = value.toLowerCase();
+    //     const symbol0 = pair.token0.symbol.toLowerCase();
+    //     const symbol1 = pair.token1.symbol.toLowerCase();
+    //     const address = pair.id.toLowerCase();
 
-        return [symbol0, symbol1, pair.pairReadable, address].some(
-            (value) => value.indexOf(search) > -1
-        );
-    };
+    //     return [symbol0, symbol1, pair.pairReadable, address].some(
+    //         (value) => value.indexOf(search) > -1
+    //     );
+    // };
 
     const renderTextField = (item: string | UniswapPair) => {
         if (typeof item === 'string' || !item) {
@@ -68,21 +95,38 @@ function PairSearch({
     };
 
     return (
-        <Combobox
-            // @ts-expect-error: className is not on the props definition but does propagate to component
-            className='pair-search'
-            placeholder='Search for pairs and tokens by symbol or address...'
-            busy
-            busySpinner={<FontAwesomeIcon icon={faSearch} />}
-            data={pairs}
-            textField={renderTextField}
-            value={currentValue}
-            itemComponent={({ item }) => <PairWithLogo pair={item} />}
-            // itemComponent={({ item }) => { console.log('THIS IS ITEM', item); return <div />; }}
-            filter={lookForPair}
-            caseSensitive={false}
-            onChange={setCurrentValue}
-        />
+        // <Combobox
+        //     // @ts-expect-error: className is not on the props definition but does propagate to component
+        //     className='pair-search'
+        //     placeholder='Search for pairs and tokens by symbol or address...'
+        //     busy
+        //     busySpinner={<FontAwesomeIcon icon={faSearch} />}
+        //     data={pairs}
+        //     textField={renderTextField}
+        //     value={currentValue}
+        //     itemComponent={({ item }) => <div>{'foo'}</div>}
+        //     // itemComponent={({ item }) => { console.log('THIS IS ITEM', item); return <div />; }}
+        //     filter={lookForPair}
+        //     caseSensitive={false}
+        //     onChange={setCurrentValue}
+        // />
+        <Combobox aria-label='pair-search'>
+            <ComboboxInput
+                onChange={(ev) => setCurrentValue(ev.target.value)}
+            />
+            {matchingPairs && (
+                <ComboboxPopover>
+                    <ComboboxList>
+                        {matchingPairs.map((pair, index) => (
+                            <ComboboxOption
+                                key={index}
+                                value={pair.pairReadable}
+                            />
+                        ))}
+                    </ComboboxList>
+                </ComboboxPopover>
+            )}
+        </Combobox>
     );
 }
 
