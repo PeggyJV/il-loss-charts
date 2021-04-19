a4ce286022c44587f288339a35d0cc826a591a59
khcoaching <khcoaching@github>
2021-04-19 12:45:25 +0900

    introduce graphql codegen

---
+++ packages/server/src/services/uniswap-v3/apollo-client.ts
@@ -0,0 +1,74 @@
+import { ApolloClient, QueryOptions, MutationOptions } from '@apollo/client/core';
+import { DocumentNode } from 'graphql';
+
+import { getSdk, Requester } from './generated-types';
+
+export type ApolloRequesterOptions<V, R> =
+  | Omit<QueryOptions<V>, 'variables' | 'query'>
+  | Omit<MutationOptions<R, V>, 'variables' | 'mutation'>;
+
+const validDocDefOps = ['mutation', 'query', 'subscription'];
+
+export default function getSdkApollo<C>(client: ApolloClient<C>) {
+  const requester: Requester = async <R, V>(
+    doc: DocumentNode,
+    variables: V,
+    options?: ApolloRequesterOptions<V, R>,
+  ): Promise<R> => {
+    // Valid document should contain *single* query or mutation unless it's has a fragment
+    if (
+      doc.definitions.filter(
+        d =>
+          d.kind === 'OperationDefinition' &&
+          validDocDefOps.includes(d.operation),
+      ).length !== 1
+    ) {
+      throw new Error(
+        'DocumentNode passed to Apollo Client must contain single query or mutation',
+      );
+    }
+
+    const definition = doc.definitions[0];
+
+    // Valid document should contain *OperationDefinition*
+    if (definition.kind !== 'OperationDefinition') {
+      throw new Error(
+        'DocumentNode passed to Apollo Client must contain single query or mutation',
+      );
+    }
+
+    switch (definition.operation) {
+      case 'query': {
+        const response = await client.query<R, V>({
+          query: doc,
+          variables,
+          ...options,
+        });
+
+        if (response.errors) {
+          throw new Error(JSON.stringify(response.errors));
+        }
+
+        if (response.data === undefined || response.data === null) {
+          throw new Error('No data presented in the GraphQL response');
+        }
+
+        return response.data;
+      }
+      case 'mutation': {
+        throw new Error(
+          'Subscription requests through SDK interface are not supported',
+        );
+      }
+      case 'subscription': {
+        throw new Error(
+          'Subscription requests through SDK interface are not supported',
+        );
+      }
+    }
+  };
+
+  return getSdk(requester);
+}
+
+export type Sdk = ReturnType<typeof getSdkApollo>;
---
+++ packages/server/src/services/uniswap-v3/generated-types.ts
@@ -0,0 +1,401 @@
+import { DocumentNode } from 'graphql';
+import gql from 'graphql-tag';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  BigDecimal: any;
+  BigInt: any;
+  Bytes: any;
+};
+
+
+
+export type Bundle = {
+  __typename?: 'Bundle';
+  id: Scalars['ID'];
+  ethPrice: Scalars['BigDecimal'];
+};
+
+export type Burn = {
+  __typename?: 'Burn';
+  id: Scalars['ID'];
+  transaction: Transaction;
+  timestamp: Scalars['BigInt'];
+  pool: Pool;
+  liquidity: Scalars['BigDecimal'];
+  sender?: Maybe<Scalars['Bytes']>;
+  amount0: Scalars['BigDecimal'];
+  amount1: Scalars['BigDecimal'];
+  tickLower: Scalars['BigInt'];
+  tickUpper: Scalars['BigInt'];
+  to?: Maybe<Scalars['Bytes']>;
+  logIndex?: Maybe<Scalars['BigInt']>;
+  amountUSD?: Maybe<Scalars['BigDecimal']>;
+  needsComplete: Scalars['Boolean'];
+  feeTo?: Maybe<Scalars['Bytes']>;
+  feeLiquidity?: Maybe<Scalars['BigDecimal']>;
+};
+
+
+export type Collect = {
+  __typename?: 'Collect';
+  id: Scalars['ID'];
+  transaction: Transaction;
+  timestamp: Scalars['BigInt'];
+  pool: Pool;
+  sender?: Maybe<Scalars['Bytes']>;
+  amount0: Scalars['BigDecimal'];
+  amount1: Scalars['BigDecimal'];
+  tickLower: Scalars['BigInt'];
+  tickUpper: Scalars['BigInt'];
+  to?: Maybe<Scalars['Bytes']>;
+  logIndex?: Maybe<Scalars['BigInt']>;
+  amountUSD?: Maybe<Scalars['BigDecimal']>;
+};
+
+export type Factory = {
+  __typename?: 'Factory';
+  id: Scalars['ID'];
+  poolCount: Scalars['BigInt'];
+  totalVolumeUSD: Scalars['BigDecimal'];
+  totalVolumeETH: Scalars['BigDecimal'];
+  untrackedVolumeUSD: Scalars['BigDecimal'];
+  combinedVolumeUSD: Scalars['BigDecimal'];
+  totalLiquidityUSD: Scalars['BigDecimal'];
+  totalLiquidityETH: Scalars['BigDecimal'];
+  txCount: Scalars['BigInt'];
+};
+
+export type Mint = {
+  __typename?: 'Mint';
+  id: Scalars['ID'];
+  transaction: Transaction;
+  timestamp: Scalars['BigInt'];
+  pool: Pool;
+  to: Scalars['Bytes'];
+  liquidity: Scalars['BigDecimal'];
+  sender?: Maybe<Scalars['Bytes']>;
+  amount0: Scalars['BigDecimal'];
+  amount1: Scalars['BigDecimal'];
+  tickLower: Scalars['BigInt'];
+  tickUpper: Scalars['BigInt'];
+  logIndex?: Maybe<Scalars['BigInt']>;
+  amountUSD?: Maybe<Scalars['BigDecimal']>;
+  feeTo?: Maybe<Scalars['Bytes']>;
+  feeLiquidity?: Maybe<Scalars['BigDecimal']>;
+};
+
+export type Pool = {
+  __typename?: 'Pool';
+  id: Scalars['ID'];
+  token0: Token;
+  token1: Token;
+  feeTier: Scalars['BigInt'];
+  tickSpacing: Scalars['BigInt'];
+  liquidity: Scalars['BigDecimal'];
+  sqrtPrice: Scalars['BigDecimal'];
+  reserve0: Scalars['BigDecimal'];
+  reserve1: Scalars['BigDecimal'];
+  reserveETH: Scalars['BigDecimal'];
+  reserveUSD: Scalars['BigDecimal'];
+  trackedReserveETH: Scalars['BigDecimal'];
+  token0Price: Scalars['BigDecimal'];
+  token1Price: Scalars['BigDecimal'];
+  volumeToken0: Scalars['BigDecimal'];
+  volumeToken1: Scalars['BigDecimal'];
+  volumeUSD: Scalars['BigDecimal'];
+  untrackedVolumeUSD: Scalars['BigDecimal'];
+  combinedVolumeUSD: Scalars['BigDecimal'];
+  txCount: Scalars['BigInt'];
+  uncollectedFeesToken0: Scalars['BigDecimal'];
+  uncollectedFeesToken1: Scalars['BigDecimal'];
+  uncollectedFeesUSD: Scalars['BigDecimal'];
+  collectedFeesToken0: Scalars['BigDecimal'];
+  collectedFeesToken1: Scalars['BigDecimal'];
+  collectedFeesUSD: Scalars['BigDecimal'];
+  createdAtTimestamp: Scalars['BigInt'];
+  createdAtBlockNumber: Scalars['BigInt'];
+  liquidityProviderCount: Scalars['BigInt'];
+  poolHourData: Array<PoolHourData>;
+  poolDayData: Array<PoolDayData>;
+  mints: Array<Mint>;
+  burns: Array<Burn>;
+  swaps: Array<Swap>;
+  collects: Array<Collect>;
+  ticks: Array<Tick>;
+};
+
+export type PoolDayData = {
+  __typename?: 'PoolDayData';
+  id: Scalars['ID'];
+  date: Scalars['Int'];
+  pool: Pool;
+  token0: Token;
+  token1: Token;
+  liquidity: Scalars['BigDecimal'];
+  sqrtPrice: Scalars['BigDecimal'];
+  reserve0: Scalars['BigDecimal'];
+  reserve1: Scalars['BigDecimal'];
+  reserveUSD: Scalars['BigDecimal'];
+  periodVolumeToken0: Scalars['BigDecimal'];
+  periodVolumeToken1: Scalars['BigDecimal'];
+  periodVolumeUSD: Scalars['BigDecimal'];
+  periodTxCount: Scalars['BigInt'];
+};
+
+export type PoolHourData = {
+  __typename?: 'PoolHourData';
+  id: Scalars['ID'];
+  periodStartUnix: Scalars['Int'];
+  pool: Pool;
+  token0: Token;
+  token1: Token;
+  liquidity: Scalars['BigDecimal'];
+  sqrtPrice: Scalars['BigDecimal'];
+  reserve0: Scalars['BigDecimal'];
+  reserve1: Scalars['BigDecimal'];
+  reserveUSD: Scalars['BigDecimal'];
+  periodVolumeToken0: Scalars['BigDecimal'];
+  periodVolumeToken1: Scalars['BigDecimal'];
+  periodVolumeUSD: Scalars['BigDecimal'];
+  periodTxCount: Scalars['BigInt'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  token?: Maybe<Token>;
+  pool?: Maybe<Pool>;
+};
+
+
+export type QueryTokenArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type QueryPoolArgs = {
+  id: Scalars['ID'];
+};
+
+export type Swap = {
+  __typename?: 'Swap';
+  id: Scalars['ID'];
+  transaction: Transaction;
+  timestamp: Scalars['BigInt'];
+  pool: Pool;
+  sender: Scalars['Bytes'];
+  from: Scalars['Bytes'];
+  amount0In: Scalars['BigDecimal'];
+  amount1In: Scalars['BigDecimal'];
+  amount0Out: Scalars['BigDecimal'];
+  amount1Out: Scalars['BigDecimal'];
+  sqrtPriceX96: Scalars['BigDecimal'];
+  tick: Tick;
+  to: Scalars['Bytes'];
+  logIndex?: Maybe<Scalars['BigInt']>;
+  amountUSD: Scalars['BigDecimal'];
+};
+
+export type Tick = {
+  __typename?: 'Tick';
+  id: Scalars['ID'];
+  pool: Pool;
+  sqrtPriceLower: Scalars['BigDecimal'];
+  sqrtPriceUpper: Scalars['BigDecimal'];
+  isActive: Scalars['Boolean'];
+  reserve0: Scalars['BigDecimal'];
+  reserve1: Scalars['BigDecimal'];
+  reserveETH: Scalars['BigDecimal'];
+  reserveUSD: Scalars['BigDecimal'];
+  trackedReserveETH: Scalars['BigDecimal'];
+  volumeToken0: Scalars['BigDecimal'];
+  volumeToken1: Scalars['BigDecimal'];
+  volumeUSD: Scalars['BigDecimal'];
+  untrackedVolumeUSD: Scalars['BigDecimal'];
+  txCount: Scalars['BigInt'];
+  uncollectedFeesToken0: Scalars['BigDecimal'];
+  uncollectedFeesToken1: Scalars['BigDecimal'];
+  uncollectedFeesUSD: Scalars['BigDecimal'];
+  collectedFeesToken0: Scalars['BigDecimal'];
+  collectedFeesToken1: Scalars['BigDecimal'];
+  collectedFeesUSD: Scalars['BigDecimal'];
+  createdAtTimestamp: Scalars['BigInt'];
+  createdAtBlockNumber: Scalars['BigInt'];
+  liquidityProviderCount: Scalars['BigInt'];
+  swaps: Array<Swap>;
+};
+
+export type TickDayData = {
+  __typename?: 'TickDayData';
+  id: Scalars['ID'];
+  date: Scalars['Int'];
+  pool: Pool;
+  tick: Tick;
+  token0: Token;
+  token1: Token;
+  liquidity: Scalars['BigDecimal'];
+  sqrtPrice: Scalars['BigDecimal'];
+  reserve0: Scalars['BigDecimal'];
+  reserve1: Scalars['BigDecimal'];
+  reserveUSD: Scalars['BigDecimal'];
+  periodVolumeToken0: Scalars['BigDecimal'];
+  periodVolumeToken1: Scalars['BigDecimal'];
+  periodVolumeUSD: Scalars['BigDecimal'];
+  periodTxCount: Scalars['BigInt'];
+};
+
+export type TickHourData = {
+  __typename?: 'TickHourData';
+  id: Scalars['ID'];
+  periodStartUnix: Scalars['Int'];
+  pool: Pool;
+  tick: Tick;
+  token0: Token;
+  token1: Token;
+  liquidity: Scalars['BigDecimal'];
+  sqrtPrice: Scalars['BigDecimal'];
+  reserve0: Scalars['BigDecimal'];
+  reserve1: Scalars['BigDecimal'];
+  reserveUSD: Scalars['BigDecimal'];
+  periodVolumeToken0: Scalars['BigDecimal'];
+  periodVolumeToken1: Scalars['BigDecimal'];
+  periodVolumeUSD: Scalars['BigDecimal'];
+  periodTxCount: Scalars['BigInt'];
+};
+
+export type Token = {
+  __typename?: 'Token';
+  id: Scalars['ID'];
+  symbol: Scalars['String'];
+  name: Scalars['String'];
+  decimals: Scalars['BigInt'];
+  totalSupply: Scalars['BigInt'];
+  tradeVolume: Scalars['BigDecimal'];
+  tradeVolumeUSD: Scalars['BigDecimal'];
+  untrackedVolumeUSD: Scalars['BigDecimal'];
+  combinedVolumeUSD: Scalars['BigDecimal'];
+  txCount: Scalars['BigInt'];
+  poolCount: Scalars['BigInt'];
+  totalLiquidity: Scalars['BigDecimal'];
+  derivedETH?: Maybe<Scalars['BigDecimal']>;
+  tokenDayData: Array<TokenDayData>;
+  poolDayDataBase: Array<PoolDayData>;
+  poolDayDataQuote: Array<PoolDayData>;
+  poolBase: Array<Pool>;
+  poolQuote: Array<Pool>;
+};
+
+export type TokenDayData = {
+  __typename?: 'TokenDayData';
+  id: Scalars['ID'];
+  date: Scalars['Int'];
+  token: Token;
+  periodVolumeToken: Scalars['BigDecimal'];
+  periodVolumeETH: Scalars['BigDecimal'];
+  periodVolumeUSD: Scalars['BigDecimal'];
+  periodTxCount: Scalars['BigInt'];
+  totalLiquidityToken: Scalars['BigDecimal'];
+  totalLiquidityETH: Scalars['BigDecimal'];
+  totalLiquidityUSD: Scalars['BigDecimal'];
+  priceUSD: Scalars['BigDecimal'];
+};
+
+export type Transaction = {
+  __typename?: 'Transaction';
+  id: Scalars['ID'];
+  blockNumber: Scalars['BigInt'];
+  timestamp: Scalars['BigInt'];
+  mints: Array<Maybe<Mint>>;
+  burns: Array<Maybe<Burn>>;
+  swaps: Array<Maybe<Swap>>;
+  collects: Array<Maybe<Collect>>;
+};
+
+export type UniswapDayData = {
+  __typename?: 'UniswapDayData';
+  id: Scalars['ID'];
+  date: Scalars['Int'];
+  dailyVolumeETH: Scalars['BigDecimal'];
+  dailyVolumeUSD: Scalars['BigDecimal'];
+  dailyVolumeUntracked: Scalars['BigDecimal'];
+  totalVolumeETH: Scalars['BigDecimal'];
+  totalLiquidityETH: Scalars['BigDecimal'];
+  totalVolumeUSD: Scalars['BigDecimal'];
+  totalLiquidityUSD: Scalars['BigDecimal'];
+  txCount: Scalars['BigInt'];
+};
+
+export type GetPairOverviewQueryVariables = Exact<{
+  id: Scalars['ID'];
+}>;
+
+
+export type GetPairOverviewQuery = (
+  { __typename?: 'Query' }
+  & { pool?: Maybe<(
+    { __typename?: 'Pool' }
+    & Pick<Pool, 'id' | 'reserve0' | 'reserve1' | 'reserveUSD' | 'trackedReserveETH' | 'token0Price' | 'token1Price' | 'volumeUSD' | 'untrackedVolumeUSD' | 'liquidity' | 'txCount' | 'createdAtTimestamp'>
+    & { token0: (
+      { __typename?: 'Token' }
+      & Pick<Token, 'id' | 'name' | 'symbol' | 'decimals' | 'derivedETH' | 'tradeVolumeUSD' | 'totalLiquidity'>
+    ), token1: (
+      { __typename?: 'Token' }
+      & Pick<Token, 'id' | 'symbol' | 'name' | 'decimals' | 'derivedETH' | 'tradeVolumeUSD' | 'totalLiquidity'>
+    ) }
+  )> }
+);
+
+
+export const GetPairOverviewDocument = gql`
+    query GetPairOverview($id: ID!) {
+  pool(id: $id) {
+    id
+    token0 {
+      id
+      name
+      symbol
+      decimals
+      derivedETH
+      tradeVolumeUSD
+      totalLiquidity
+    }
+    token1 {
+      id
+      symbol
+      name
+      decimals
+      derivedETH
+      tradeVolumeUSD
+      totalLiquidity
+    }
+    reserve0
+    reserve1
+    reserveUSD
+    trackedReserveETH
+    token0Price
+    token1Price
+    volumeUSD
+    untrackedVolumeUSD
+    liquidity
+    txCount
+    createdAtTimestamp
+  }
+}
+    `;
+export type Requester<C= {}> = <R, V>(doc: DocumentNode, vars?: V, options?: C) => Promise<R>
+export function getSdk<C>(requester: Requester<C>) {
+  return {
+    GetPairOverview(variables: GetPairOverviewQueryVariables, options?: C): Promise<GetPairOverviewQuery> {
+      return requester<GetPairOverviewQuery, GetPairOverviewQueryVariables>(GetPairOverviewDocument, variables, options);
+    }
+  };
+}
+export type Sdk = ReturnType<typeof getSdk>;
