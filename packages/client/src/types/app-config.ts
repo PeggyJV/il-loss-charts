38fe8b8e94910089c75c28c54fa24265c289767b
Lucky Odisetti <lucky.odisetti@gmail.com>
2021-04-22 01:44:47 -0700

    add network config

--- packages/client/src/types/app-config.ts
+++ packages/client/src/types/app-config.ts
@@ -1,5 +1,25 @@
 export type Environments = 'production' | 'development' | 'test' | 'staging';
 
+export type NetworkNamespace = 'mainnet' | 'rinkeby' | 'goerli' | 'ropsten' | 'kovan';
+export type NetworkIds = '1' | '4' | '5' | '3' | '42'
+type ContractABI =
+    | 'ADD_LIQUIDITY'
+    | 'REMOVE_LIQUIDITY'
+    | 'TWO_SIDE_ADD_LIQUIDITY'
+    | 'ADD_LIQUIDITY_V3';
+
+type Network = {
+    [key in NetworkIds]: {
+        id: number;
+        name: NetworkNamespace;
+        contracts?: Partial<
+            {
+                [key in ContractABI]: string;
+            }
+        >;
+    };
+};
 export default interface AppConfig {
     wsApi: string;
+    networks: Network;
 }
