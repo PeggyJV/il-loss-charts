da1178271723d8c7a91a1a5e85a40d20c6323279
khcoaching <82778544+khcoaching@users.noreply.github.com>
2021-05-12 11:38:56 +0900

    caching for v2 routes (#115)
    Co-authored-by: khcoaching <khcoaching@github>

--- packages/server/src/api/util/wrap-request.ts
+++ packages/server/src/api/util/wrap-request.ts
@@ -1,13 +1,22 @@
 import { Request, Response, RequestHandler } from 'express';
 
+import { CacheControl, setCacheControl } from 'api/util/set-cache-control';
+
 export default function wrapRequest(
     // eslint-ignore-next-line no-unused-vars
-    controllerFn: (req: Request<any, any, any, any>, res: Response) => Promise<unknown>
+    controllerFn: (req: Request<any, any, any, any>, res: Response) => Promise<unknown>,
+    cacheOptions?: CacheControl,
 ): RequestHandler {
     return async (req: Request, res: Response) => {
         try {
-            const response = await controllerFn(req, res);
-            return res.json({ data: response });
+            const data = await controllerFn(req, res);
+
+            // set cache control headers if configured
+            if (cacheOptions) {
+                setCacheControl(res, cacheOptions);
+            }
+
+            return res.json({ data });
         } catch (err) {
             console.error('Error:', err);
             return res.status(err.status || 500).json({ error: err.message });
