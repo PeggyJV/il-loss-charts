c534f0d5a71d04e6d46bd93d2bde5cf196a3e195
Lucky Odisetti <lucky.odisetti@gmail.com>
2021-05-19 04:58:53 -0700

    create info modal (#133)

--- packages/client/src/components/connect-wallet-modal.tsx
+++ packages/client/src/components/connect-wallet-modal.tsx
@@ -39,7 +39,6 @@ function ConnectWalletModal({
     };
 
     const handleConnectWalletConnect = async () => {
-        console.log('handle connect wallet');
         try {
             await connectWalletConnect();
         } catch (err) {
