eab44b8e49126fae8b7e12c1343f6a28f8dec701
khcoaching <khcoaching@github>
2021-05-22 09:11:08 +0900

    short link api

--- packages/server/src/api/util/set-cache-control.ts
+++ packages/server/src/api/util/set-cache-control.ts
@@ -4,6 +4,7 @@ export type CacheControl = {
     // cacheability options
     public?: boolean;
     noStore?: boolean;
+    immutable?: boolean;
 
     // expiration options
     maxAge?: number; // seconds
@@ -30,6 +31,7 @@ const optionToName: Record<string, string> = {
     sMaxAge: 's-maxage',
     mustRevalidate: 'must-revalidate',
     proxyRevalidate: 'proxy-revalidate',
+    immutable: 'immutable,',
 };
 
 const validCacheability: Array<keyof CacheControl> = ['public', 'noStore'];
@@ -38,6 +40,7 @@ const validDirectives: Array<keyof CacheControl> = [
     'sMaxAge',
     'mustRevalidate',
     'proxyRevalidate',
+    'immutable',
 ];
 
 export function makeDirective(options: CacheControl): string | undefined {
