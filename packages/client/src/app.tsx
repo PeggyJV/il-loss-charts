8f77c60a5a211e5e0ea0c74c95b5d31366c02670
Lucky Odisetti <lucky.odisetti@gmail.com>
2021-06-03 12:28:14 -0700

    update route

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -103,7 +103,7 @@ function App(): ReactElement {
                                             )}
                                         >
                                             <Switch>
-                                                <Route path='/:poolId'>
+                                                <Route path='/pools/:poolId'>
                                                     <LandingContainer
                                                         gasPrices={gasPrices}
                                                         setShowConnectWallet={
