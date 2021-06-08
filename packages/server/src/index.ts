97b04fbcce8cc69d3f6e9823a486e0c15f7cd534
Kevin Kennis <kevinvkennis@gmail.com>
2021-06-08 10:50:33 -0700

    fix build issue

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,7 +5,7 @@ import { calculateMarketStats } from 'util/calculate-stats';
 import { UniswapV3Fetcher } from 'services/uniswap-v3/fetcher';
 import UniswapFetcher from 'services/uniswap';
 import BitqueryFetcher from 'services/bitquery/fetcher';
-import { _getPeriodIndicators } from 'api/controllers/market-data';
+import { _getBitqueryPeriodIndicators as _getPeriodIndicators } from 'api/controllers/market-data';
 
 export {
     _getPeriodIndicators,
