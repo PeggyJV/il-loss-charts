43525cbda51cc3499f4bb955bbaa69b1a4028f95
Kevin Kennis <kevinvkennis@gmail.com>
2021-06-09 08:27:54 -0700

    change config path

--- packages/server/src/config/default.ts
+++ packages/server/src/config/default.ts
@@ -52,7 +52,7 @@ const config = {
     pools: {
         shortLinkBaseUrl: 'https://dev.somm.fi',
         deepLinkBaseUrl: defer(function (): string {
-            const host: string = this.server.host ?? '';
+            const host: string = this.default?.server?.host ?? '';
             return `${host}/pools`;
         }),
     },
