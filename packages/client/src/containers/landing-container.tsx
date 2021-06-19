0b8c86bc45df0ce77594676b812a85a7d2a99c50
Lucky Odisetti <lucky.odisetti@gmail.com>
2021-06-19 00:28:09 -0700

    add sidebar with socials

--- packages/client/src/containers/landing-container.tsx
+++ packages/client/src/containers/landing-container.tsx
@@ -1,6 +1,7 @@
 import { EthGasPrices } from '@sommelier/shared-types';
-import { TelegramCTA } from 'components/telegram-cta';
+import { TelegramCTA, TwitterCTA, BlogCTA } from 'components/social-cta';
 import { LiquidityContainer } from 'containers/liquidity-container';
+import { useMediaQuery } from 'react-responsive';
 import { AppHeader } from 'components/app-header/app-header';
 import { Box } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,15 +11,37 @@ function LandingContainer({
 }: {
     gasPrices: EthGasPrices | null;
 }): JSX.Element {
+    const isMobile = useMediaQuery({ query: '(max-width: 800px)' });
+
     return (
         <div>
             <AppHeader />
             <Box
                 display='flex'
-                flexDirection='column'
-                alignItems='center'
-                justifyContent='space-around'
+                flexDirection='row'
+                alignItems='flex-start'
+                justifyContent='space-between'
             >
+                {!isMobile && (
+                    <Box
+                        style={{
+                            background: 'var(--bgPrimary)',
+                            padding: '1.5rem 2rem',
+                            borderRadius: '8px',
+                            maxWidth: '220px',
+                            fontSize: '1.15rem',
+                            boxShadow: '0 3px 12px var(--bgDeep)',
+                        }}
+                    >
+                        <p>The easiest way to add liquidity on Uniswap v3</p>
+                        <br />
+                        <Box>
+                            <TelegramCTA />
+                            <TwitterCTA />
+                            <BlogCTA />
+                        </Box>
+                    </Box>
+                )}
                 <LiquidityContainer gasPrices={gasPrices} />
             </Box>
             <Box
@@ -26,16 +49,6 @@ function LandingContainer({
                 alignItems='center'
                 className='footer-tab-container'
             >
-                {/* <a href='https://t.me/getsomm' target='_blank' rel='noreferrer'>
-                    <FontAwesomeIcon icon={faTelegram} />
-                </a>
-                <a
-                    href='https://twitter.com/sommfinance'
-                    target='_blank'
-                    rel='noreferrer'
-                >
-                    <FontAwesomeIcon icon={faTwitter} />
-                </a> */}
                 <a
                     className='support-tab'
                     href='https://discord.gg/VXyUgtnbtv'
